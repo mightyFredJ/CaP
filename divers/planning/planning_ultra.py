8525b2b404b3ba60c5bb0e136a332f8755351a30
FredJ <none@none>
2017-01-09 09:19:13 +0100

    gestion des vacances microretouches esthétiques

--- divers/planning/planning_ultra.py
+++ divers/planning/planning_ultra.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
+from matplotlib.patches import Ellipse, Polygon
 from matplotlib.lines import Line2D
 
 # ---- mine ----
@@ -72,7 +72,7 @@ prepa = {
              23],
     'pre':  [33, 44, 28, 33],
 }
-
+#
 #ref_prepa = {
 #    'data': [80,  20, 100,
 #             20,  60, 100,
@@ -97,6 +97,12 @@ intermediaires = [
         'dist': 55,
         'D+':   1000,
     },
+]
+
+vacances = [
+    ('4/2', '12/2'),
+    ('8/4', '17/4'),
+    ('12/8', '27/8'),
 ]
 
 #
@@ -149,74 +155,74 @@ intermediaires = [
 #]
 #
 #
-## ---- UT4M ----
-#
-#objectif = {
-#    'name': 'UT4M',
-#    'date': datetime(2017, 8, 17),
-#    'dist': 169,
-#    'D+':   11000,  # diff 280
-#    'prepamax': 180,
-#}
-#objectif = {
-#    'name': 'Montreux',
-#    'date': datetime(2017, 7, 28),
-#    'dist': 165,
-#    'D+':   13600,  # diff 300
-#    'prepamax': 180,
-#}
-#objectif = {
-#    'name': 'Suisse',
-#    'date': datetime(2017, 9, 1),
-#    'dist': 165,
-#    'D+':   11800,  # diff 280
-#    'prepamax': 180,
-#}
-#objectif = {
-#    'name': 'ITV',
-#    'date': datetime(2017, 9, 8),
-#    'dist': 200,
-#    'D+':   10400,  # diff 300
-#    'prepamax': 180,
-#}
-#
-#ref_prepa = {
-#    'data': [50,  65,  80,  30,
-#             60,  70,  40,
-#             80,  20,  80,  20,
-#             80,  20, 100,
-#             20,  60, 100,
-#             20,  40,  20],
-#    'labels': ['', '', 'volume', '',
-#               '', 'trail', '',
-#               'WEC', '', '', '',
-#               'ultra', '', 'WEC',
-#               '', '', 'WEC',
-#               '', '', ''],
-#}
-#
-#prepa = {
-#    # mode WEC
-#    'name': "WECs",
-#    'data': [50,  65,  80,  30,
-#             60,  70,  40,
-#             70,  20,  40,  20,
-#             80,  20, 100,
-#             20,  60, 100,
-#             20,  40,  20],
-#    'pre':  [50, 65, 60, 35, 50, 25],
-#}
-#
-#intermediaires = [
-#    { 'name': 'UTC', 'date': '13/5', 'dist': 104, 'D+': 4000, },
-#    { 'name': 'Gypaëte', 'date': '3/6', 'dist': 70, 'D+': 4000, },
-#    { 'name': 'Samoëns', 'date': '17/6', 'dist': 86,  'D+': 6000,  },
-#    { 'name': 'AMT', 'date': '24/6', 'dist': 93, 'D+': 4800, },
-#    { 'name': 'MH100', 'date': '8/7', 'dist': 108, 'D+': 8100, },
-#    { 'name': 'UTB', 'date': '22/7', 'dist': 100, 'D+': 6000, },
-#    { 'name': 'TdFiz', 'date': '30/7', 'dist': 60, 'D+': 5000, },
-#    { 'name': 'Ut4M', 'date': '18/8', 'dist': 100, 'D+': 5500, },
-#]
+# ---- UT4M ----
+
+objectif = {
+    'name': 'UT4M',
+    'date': datetime(2017, 8, 17),
+    'dist': 169,
+    'D+':   11000,  # diff 280
+    'prepamax': 180,
+}
+objectif = {
+    'name': 'Montreux',
+    'date': datetime(2017, 7, 28),
+    'dist': 165,
+    'D+':   13600,  # diff 300
+    'prepamax': 180,
+}
+objectif = {
+    'name': 'Suisse',
+    'date': datetime(2017, 9, 1),
+    'dist': 165,
+    'D+':   11800,  # diff 280
+    'prepamax': 180,
+}
+objectif = {
+    'name': 'ITV',
+    'date': datetime(2017, 9, 8),
+    'dist': 200,
+    'D+':   10400,  # diff 300
+    'prepamax': 180,
+}
+
+ref_prepa = {
+    'data': [50,  65,  80,  30,
+             60,  70,  40,
+             80,  20,  80,  20,
+             80,  20, 100,
+             20,  60, 100,
+             20,  40,  20],
+    'labels': ['', '', 'volume', '',
+               '', 'trail', '',
+               'WEC', '', '', '',
+               'ultra', '', 'WEC',
+               '', '', 'WEC',
+               '', '', ''],
+}
+
+prepa = {
+    # mode WEC
+    'name': "WECs",
+    'data': [50,  65,  80,  30,
+             60,  70,  40,
+             70,  20,  40,  20,
+             80,  20, 100,
+             20,  60, 100,
+             20,  40,  20],
+    'pre':  [50, 65, 60, 35, 50, 25],
+}
+
+intermediaires = [
+    { 'name': 'UTC', 'date': '13/5', 'dist': 104, 'D+': 4000, },
+    { 'name': 'Gypaëte', 'date': '3/6', 'dist': 70, 'D+': 4000, },
+    { 'name': 'Samoëns', 'date': '17/6', 'dist': 86,  'D+': 6000,  },
+    { 'name': 'AMT', 'date': '24/6', 'dist': 93, 'D+': 4800, },
+    { 'name': 'MH100', 'date': '8/7', 'dist': 108, 'D+': 8100, },
+    { 'name': 'UTB', 'date': '22/7', 'dist': 100, 'D+': 6000, },
+    { 'name': 'TdFiz', 'date': '30/7', 'dist': 60, 'D+': 5000, },
+    { 'name': 'Ut4M', 'date': '18/8', 'dist': 100, 'D+': 5500, },
+]
 
 
 #%% manip des données --------------------------------------------------------
@@ -224,14 +230,24 @@ intermediaires = [
 # mise en forme python des données user-friendly
 
 def ekm(km, deniv):
+    """ calcule la difficulté d'une course
+        >>> ekm(112, 2800)
+        140
+    """
     return km + deniv/100.
+
+def str2dt(s):
+    """ convertit une chaine en datetime
+        >>> str2dt('6/2')
+        datetime.datetime(2017, 2, 6, 0, 0)
+    """
+    return datetime.strptime(s + '/17', '%d/%m/%y')
 
 objectif['diff'] = ekm(objectif['dist'], objectif['D+'])
 for inter in chain([objectif], intermediaires):
     if not isinstance(inter['date'], datetime):
-        inter['date'] = datetime.strptime(inter['date'] + '/17', '%d/%m/%y')
+        inter['date'] = str2dt(inter['date'])
     inter['diff'] = ekm(inter['dist'], inter['D+'])
-
 
 # prépa
 n = len(prepa['data'])  # nb semaines prépa
@@ -247,7 +263,7 @@ weeks_labels = ["S -%d" % i for i in range(n, 0, -1)]
 print("1er lundi de la prépa : %s (%s)" % (mondays_labels[0], weeks_labels[0]))
 
 # objectif
-x_obj = objectif['date'].timestamp() - .5*sem
+x_obj = objectif['date'].timestamp()
 y_obj = ekm(objectif['dist'], objectif['D+'])
 if not 'prepamax' in objectif:
     objectif['prepamax'] = y_obj
@@ -285,7 +301,7 @@ ymax = y_obj + 20
 bw = 0.8 * sem
 if show_ref:
     bw = 0.6 * sem
-shift = 0.1 * bw
+shift = 0  #0.1 * bw
 
 
 pre_color = '0.6'
@@ -295,12 +311,14 @@ int_color = 'g'
 ref_color = '0.3'
 
 # ----  barres
-plt.bar(x_pre - bw/2.  , y_pre, color=pre_color, width=bw, ls='dashed')
-plt.bar(x - bw/2. - shift, y,     color=prepa_color, width=bw)
+
+plt.bar(x_pre , y_pre, color=pre_color, width=bw, ls='dashed')
+plt.bar(x - shift, y,     color=prepa_color, width=bw)
 if show_ref:
-    plt.bar(x - bw/2. + shift, y_ref+1, color=ref_color, width=bw, zorder=0)  # zorder : derrière
+    plt.bar(x + shift, y_ref+1, color=ref_color, width=bw, zorder=0)  # zorder : derrière
 
 # ---- objectif
+
 plt.gca().add_patch(Ellipse((x_obj, y_obj), 3*jour, 5*(ymax/100.),
                             facecolor=obj_color, zorder=20))
 plt.gca().add_line(Line2D((x_obj, x_obj), (y[-1], y_obj),
@@ -313,6 +331,7 @@ plt.annotate(s=objectif['name'], xy=(x_obj, y_obj+2.4),
 
 
 # ---- intermédiaires
+
 for inter in intermediaires:
     x_int, y_int = inter['date'].timestamp(), inter['diff']
     plt.gca().add_patch(Ellipse((x_int, y_int), 2*jour, 2*(ymax/100.),
@@ -350,7 +369,8 @@ for tickpos, label in zip(ax1.get_xticks(), upper_labels[::tickstep]):
 # ---- annotations
 
 # phase avant la préparation
-xp1, xp2 = x_pre[0] - 3*jour, x_pre[-1] + 3*jour
+esp = (sem-bw)  # entre 2 barres
+xp1, xp2 = x_pre[0] - shift - esp/2., x_pre[-1] + 1*sem - shift - esp/2.
 yp1, yp2 = pre_ymax, pre_ymax + 5
 poly = Line2D([xp1, xp1, xp2, xp2], [yp1, yp2, yp2, yp1], color=pre_color)
 plt.gca().add_line(poly)
@@ -359,7 +379,7 @@ plt.text((xp1+xp2)/2., yp2 + 5, "niveau actuel", color=pre_color, ha='center')
 if prepa['name'] == "progress":
     # mésocycles
     for i in range(int(n/4)):
-        x1, x2 = x[i*4+0] - 3*jour, x[i*4+3] + 3*jour
+        x1, x2 = x[i*4+0] - shift - esp/2., x[i*4+3] + 1*sem - shift - esp/2.
         y1 = max(y[i*4:i*4+4])
         y2 = y1 + 5
         poly = Line2D([x1, x1, x2, x2], [y1, y2, y2, y1], color=prepa_color)
@@ -371,8 +391,18 @@ elif prepa['name'] == "WECs":
     for i, lbl in enumerate(ref_prepa['labels']):
         if lbl is '':
             continue
-        plt.annotate(s=lbl, xy=(x[i]+.1*jour, y_ref[i]), xytext=(x[i]+.2, y_ref[i]+10),
+        plt.annotate(s=lbl, xy=(x[i]+.5*sem, y_ref[i]),
+                     xytext=(x[i]+.6*sem, y_ref[i]+10),
                      arrowprops=dict(arrowstyle="->"), color=ref_color)
+
+# ---- périodes de vacances
+
+for holiweek in vacances:
+    deb, fin = [str2dt(j).timestamp() for j in holiweek]
+    coords = zip((deb, deb, fin, fin), (0, ymax, ymax, 0))
+    plt.gca().add_patch(Polygon(list(coords),
+                        facecolor='w',
+                        zorder=0, hatch='/'))
 
 # ---- ça suffit pour aujourd'hui
 
